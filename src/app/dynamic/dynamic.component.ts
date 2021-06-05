a537ffa15f3d209a483b4471571dd2d9e891e060
Ambrose07 <ambrose.d.fernandes@gmail.com>
2021-06-06 00:33:20 +0530

    Add files via upload

--- src/app/dynamic/dynamic.component.ts
+++ src/app/dynamic/dynamic.component.ts
@@ -1,1 +1,38 @@
-dynamic.component.ts
+import { Component, OnInit, HostListener } from '@angular/core';
+import {  Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-dynamic',
+  templateUrl: './dynamic.component.html',
+  styleUrls: ['./dynamic.component.css']
+})
+export class DynamicComponent implements OnInit {
+
+  counter = 1;
+
+  data$: Observable<any>;
+
+  returnMessage(e){
+    if (e.origin != window.location.origin) { // set your origin
+      return false;
+    }else{
+      this.counter++;
+    }
+  }
+
+  constructor() {
+    
+  }
+
+  ngOnInit() {
+    var iframe = document.getElementById('iframe');
+    if (iframe == null) return;
+    var iWindow = (<HTMLIFrameElement>iframe).contentWindow;
+    iWindow.addEventListener('message',this.returnMessage.bind(this), false);
+  }
+
+  callParent(){
+      window.parent.postMessage({"user":"test","data":"somedata"}, window.location.origin)
+  }
+ 
+}
